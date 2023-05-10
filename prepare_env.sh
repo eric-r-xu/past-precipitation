8e7f758f91a26d70a05687d3fd39c270ed1de277
Eric Xu <eric.r.xu@gmail.com>
2023-05-10 00:31:07 -0700

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
-cd ~/
-git clone https://github.com/eric-r-xu/past-precipitation.git
 sudo apt install -y python3-pip
 sudo apt-get install python3-setuptools
-cd klaviyo-weather-app;sudo apt-get install python3-venv
+cd past-precipitation;sudo apt-get install python3-venv
 sudo apt install build-essential libssl-dev libffi-dev python-dev-is-python3
 python3 -m venv env
 . ./env/bin/activate
 python -m pip install --upgrade pip
 sudo apt-get install libsasl2-dev libsasl2-2 libsasl2-modules-gssapi-mit
 sudo apt-get install libmysqlclient-dev
-pip install -r ~/klaviyo-weather-app/py3requirements.txt
-python -m pip install git+https://gitea.ksol.io/karolyi/py3-validate-email@v1.0.9
+pip install -r ~/past-precipitation/py3requirements.txt
